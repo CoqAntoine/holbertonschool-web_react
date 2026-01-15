fcf3b4a8322aaba513763b6f8e7e64155b739d88
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 <11534@holbertonstudents.com>
2026-01-15 16:06:13 +0100

    task 3

---
+++ task_3/js/main.ts
@@ -0,0 +1,12 @@
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (
+  firstName: string,
+  lastName: string
+): string => {
+  return `${firstName.charAt(0)}. ${lastName}`;
+};
+
+console.log(printTeacher('John', 'Doe'));
